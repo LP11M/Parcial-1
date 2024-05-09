42d5135e038f42cfd57aebc57b227ad0bdf2e50c
LP11M <00426268@anahuac.mx>
2024-05-09 13:38:37 -0600

    Agregar clase alumnos

--- Ordinario2/DjangoAPI/MiApp/urls.py
+++ Ordinario2/DjangoAPI/MiApp/urls.py
@@ -5,4 +5,7 @@ from MiApp import views #Nuestros metodos
 urlpatterns=[
     path('carrera/', views.carreraApi),  # Matches /carrera/
     path('carrera/<int:id>/', views.carreraApi),  # Matches /carrera/{id}/
+
+    path('alumnos/', views.alumnosApi),  # Matches /carrera/
+    path('alumnos/<int:id>/', views.alumnosApi),  # Matches /carrera/{id}/
 ]
--- Ordinario2/DjangoAPI/MiApp/views.py
+++ Ordinario2/DjangoAPI/MiApp/views.py
@@ -8,7 +8,7 @@ from MiApp.serializers import CarreraSerializer,AlumnosSerializer
 
 # Create your views here.
 
-#creamos metodos
+#creamos metodos para carreras
 @csrf_exempt
 def carreraApi(request,id=0):
     if request.method=='GET': #Metodo get
@@ -33,4 +33,30 @@ def carreraApi(request,id=0):
     elif request.method=='DELETE': #Metodo delete
         carrera = Carreras.objects.get(IdCarrera=id)#primero recuperamos la info actual
         carrera.delete()
+        return JsonResponse("Borrado con exito",safe=False)
+    
+@csrf_exempt
+def alumnosApi(request,id=0):
+    if request.method=='GET': #Metodo get
+        alumnos = Alumnos.objects.all()
+        alumnos_serializer=AlumnosSerializer(alumnos,many=True)#Convertir a Json
+        return JsonResponse(alumnos_serializer.data,safe=False)
+    elif request.method=='POST': #Metodo post
+        alumnos_data=JSONParser().parse(request)
+        alumnos_serializer=AlumnosSerializer(data=alumnos_data)
+        if alumnos_serializer.is_valid():#Guardamos en base
+            alumnos_serializer.save()
+            return JsonResponse("Datos anadidos",safe=False)
+        return JsonResponse("No se pudo anadir",safe=False)
+    elif request.method=='PUT': #Metodo put para actualizar
+        alumnos_data=JSONParser().parse(request)
+        alumnos = Alumnos.objects.get(IdAlumno=alumnos_data['IdAlumno'])#primero recuperamos la info actual
+        alumnos_serializer=AlumnosSerializer(alumnos,data=alumnos_data) #Actualizamos
+        if alumnos_serializer.is_valid():#Guardamos en base
+            alumnos_serializer.save()
+            return JsonResponse("Actualizado",safe=False)
+        return JsonResponse("No se pudo actualizar",safe=False)
+    elif request.method=='DELETE': #Metodo delete
+        alumnos = Alumnos.objects.get(IdAlumno=id)#primero recuperamos la info actual
+        alumnos.delete()
         return JsonResponse("Borrado con exito",safe=False)
